a47420d57159166570d867f364807d6fc5508683
Ahsan Farooq <mahsanfarooq97@gmail.com>
2025-05-29 00:38:21 +0500

    Implement Snackbar context for global message handling and update components to use it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Container from '@mui/material/Container';
+import React from "react";
+import Container from "@mui/material/Container";
 
-import Header from './Header';
-import Content from './Content';
+import Header from "./Header";
+import Content from "./Content";
+import { SnackbarProvider } from "./components/Context/SnackbarContext";
 
 function App() {
   return (
-    <>
+    <SnackbarProvider>
       <Header />
       <Container>
         <Content />
       </Container>
-    </>
+    </SnackbarProvider>
   );
 }
 
